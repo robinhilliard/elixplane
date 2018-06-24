ec69de2aada3be2bbbf71b08d3f79f5bd576a28a
Robin Hilliard <robin@rocketboots.com>
2018-06-24 14:19:02 +1000

    Adding Access behaviour and Enumerable protocol

--- lib/xplane_data.ex
+++ lib/xplane_data.ex
@@ -1,14 +1,12 @@
 defmodule XPlane.Data do
   @moduledoc """
-  Get and set X-Plane data references.
+  Get and set X-Plane data.
   """
   
-  defstruct [
-    instance: nil,
-    next_code: 0, # Next unique code number
-    values: %{},  # Just the raw message, do lazy decoding
-    drefs: %{}    # map dref strings to codes
-  ]
+  defstruct [:type, :value]
+  @type t :: %XPlane.Data{
+              type: XPlane.DRef.xtype,
+              value: binary}
   
   
   @startup_grace_period 1000
@@ -37,7 +35,7 @@ defmodule XPlane.Data do
   
   @doc """
   Request updates of the specified data references at the corresponding frequency.
-  Values can then be retrieved using `XPlane.Data.get_latest_updates/2`. A small
+  Values can then be retrieved using `XPlane.Data.latest_updates/2`. A small
   delay occurs during the call to give the GenServer a chance to collect at least
   one value for each requested data reference.
   
@@ -52,7 +50,7 @@ defmodule XPlane.Data do
   :ok
   ```
   """
-  @spec request_updates(XPlane.Instance.t, list({atom, integer}))
+  @spec request_updates(XPlane.Instance.t, list({atom, integer})) :: :ok | {:error, {:invalid_drefs, list}}
   def request_updates(instance, dref_id_freq) do
     GenServer.cast(name(instance), {:request_updates, dref_id_freq})
     :timer.sleep(@startup_grace_period)  # Allow time for data ref values to be received
@@ -115,6 +113,52 @@ defmodule XPlane.Data do
   
   defp name(instance) do
     "#{__MODULE__}_#{instance.addr}"
+  end
+  
+  
+  # Access behaviour for XPlane.Data struct
+  
+  @behaviour Access
+
+  def fetch(%XPlane.Data{type: {:int, [dim]}, value: value}, index)
+    when index in 0..(dim - 1) do
+    {:ok, 0} # TODO decode int
+  end
+  
+  # TODO other xtypes
+  
+  def fetch(%XPlane.Data{type: {xtype, [dim | inner_dims]}, value: value}, index)
+    when index in 0..(dim - 1) do
+  {:ok, %XPlane.Data{type: {xtype, [inner_dims]}, value: value}} # TODO munge value
+  end
+  
+  
+  def get(data=%XPlane.Data{type: {_, [dim | _]}, value: _}, index, _)
+    when index in 0..(dim - 1) do
+    fetch(data, index)
+  end
+    
+  def get(_, _, default) do
+    default
+  end
+  
+  # Don't think we want to implement get_and_update() or pop()?
+    
+
+  
+  
+  defimpl Enumerable do
+    def count(%XPlane.Data{type: {:_, [dim | _]}, value: _}) do
+      {:ok, dim}
+    end
+    
+    
+    def member?(%XPlane.Data{type: {:_, [dim | _]}, value: _}, index) do
+      {:ok, index in 0..(dim - 1)}
+    end
+    
+    
+    # TODO reduce see p274
   end
   
 
